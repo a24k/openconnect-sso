dbb5946580b309d8bdd4a7fab689f9ca1a1a3c37
Atsushi KAWASAKI <ak@xlix.org>
2022-02-28 17:26:58 +0900

    fix: modify chrome_options

--- openconnect_sso/browser/browser.py
+++ openconnect_sso/browser/browser.py
@@ -30,8 +30,8 @@ class Browser:
         capabilities = DesiredCapabilities.CHROME
 
         if self.display_mode == DisplayMode.HIDDEN:
-            chrome_options.add_argument("headless")
-            chrome_options.add_argument("no-sandbox")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument('--disable-dev-shm-usage')
 
         if self.proxy:
